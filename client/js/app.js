7c1a60a877eb9d1e9e579efbf4aab5fb8321d606
Philippe MILINK - Mac <philippe.milink@gmx.fr>
2016-11-01 16:40:54 +0100

    Move JS parameters in a specific file

--- client/js/app.js
+++ client/js/app.js
@@ -1,8 +1,3 @@
-var ROOT_URL = "http://localhost/bookmarks/server/web/app_dev.php/";
-var CLIENT_ID = "1_3bcbxd9e24g0gk4swg0kwgcwg4o8k8g4g888kwc44gcc0gwwk4";
-var CLIENT_SECRET = "4ok2x70rlfokc8g0wws8c8kwcokw80k44sg48goc0ok4w0so0k";
-
-
 var app = angular.module('BookmarksApp', ['ngRoute', 'LocalStorageModule']);
 
 app.config(function($routeProvider) {
